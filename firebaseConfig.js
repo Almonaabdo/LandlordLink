78f8eb3e24b4c0ce27fae8347d43db97a7ec2cbd
Yafet2557 <ytekleab2557@conestogac.on.ca>
2024-10-17 18:18:15 -0400

    Hid api keys

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,17 +5,28 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Importing environment variables
+import { 
+  FIREBASE_API_KEY, 
+  FIREBASE_AUTH_DOMAIN, 
+  FIREBASE_PROJECT_ID, 
+  FIREBASE_STORAGE_BUCKET, 
+  FIREBASE_MESSAGING_SENDER_ID, 
+  FIREBASE_APP_ID, 
+  FIREBASE_MEASUREMENT_ID 
+} from 'react-native-dotenv';
+
+// Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA8rGWs81f3EWgNpdZaGahFjT1q8o25D50",
-  authDomain: "accommod8u-dc7cc.firebaseapp.com",
-  projectId: "accommod8u-dc7cc",
-  storageBucket: "accommod8u-dc7cc.appspot.com",
-  messagingSenderId: "72934641381",
-  appId: "1:72934641381:web:9da745b69ce39a03f90e0c",
-  measurementId: "G-XWEJEZXTBX"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
